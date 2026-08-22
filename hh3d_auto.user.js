b8eb8a4a3025c7b3f4c1a4b3c58792215c9f172f
thucvip83-tech <thucvip83@gmail.com>
2026-08-22 19:25:36 +0700

    Update hh3d_auto.user.js

--- hh3d_auto.user.js
+++ hh3d_auto.user.js
@@ -4,8 +4,8 @@
     // @version       5.9.9
     // @description   Auto  HH3D
     // @author        Cre: [Unknown] - Edited by VM
-    // @include       *://hoathinh3d.so*/*
-    // @exclude       *://hoathinh3d.so/khoang-mach*
+    // @include       *://hoathinh3d.ad*/*
+    // @exclude       *://hoathinh3d.ad/khoang-mach*
     // @require       https://cdn.jsdelivr.net/npm/sweetalert2@11.26.12/dist/sweetalert2.all.min.js
     // @run-at        document-start
     // @grant         unsafeWindow
